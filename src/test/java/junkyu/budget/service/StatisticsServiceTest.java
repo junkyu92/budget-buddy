e077429f1409588b866f53e5d37bd709c0b06a57
kkzz0001 <kkzz0001@gmail.com>
2023-11-14 03:02:09 +0900

    test: 실패 테스트코드 추가

--- src/test/java/junkyu/budget/service/StatisticsServiceTest.java
+++ src/test/java/junkyu/budget/service/StatisticsServiceTest.java
@@ -1,6 +1,8 @@
 package junkyu.budget.service;
 
 import junkyu.budget.dto.SpendGetResponseDto;
+import junkyu.budget.exception.CustomException;
+import junkyu.budget.exception.ErrorCode;
 import junkyu.budget.repository.SpendRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,6 +42,28 @@ class StatisticsServiceTest {
         assertThat(result.get(0).getAmount()).isEqualTo(50);
         assertThat(result.get(1).getAmount()).isEqualTo(200);
         assertThat(result.get(2).getAmount()).isEqualTo(84);
+    }
+
+    @Test
+    void 통계_실패_지난_달_데이터가_존재하지_않습니다(){
+        List<SpendGetResponseDto> lastMonth = new ArrayList<>();
+
+        when(spendRepository.lastMonthDayOfMonthSpend(999999999L)).thenReturn(lastMonth);
+
+        assertThatThrownBy(() -> statisticsService.compareToLastMonth(999999999L))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.LAST_MONTH_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    void 통계_실패_지난_요일_데이터가_존재하지_않습니다(){
+        List<SpendGetResponseDto> lastDayOfWeek = new ArrayList<>();
+
+        when(spendRepository.lastDayOfWeekSpend(999999999L)).thenReturn(lastDayOfWeek);
+
+        assertThatThrownBy(() -> statisticsService.compareToLastDayOfWeek(999999999L))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.LAST_DAY_OF_WEEK_NOT_FOUND.getMessage());
     }
 
     @Test
